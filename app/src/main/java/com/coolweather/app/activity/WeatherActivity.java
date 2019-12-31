63eedadfdacc43c5d49c126ac98a70b3b713d3bb
wwk007 <1263195113@qq.com>
2019-12-31 22:47:09 +0800

    add show weatherInfo feature

---
+++ app/src/main/java/com/coolweather/app/activity/WeatherActivity.java
@@ -0,0 +1,127 @@
+package com.coolweather.app.activity;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+
+import com.coolweather.app.R;
+import com.coolweather.app.util.HttpCallbackListener;
+import com.coolweather.app.util.HttpUtil;
+import com.coolweather.app.util.Utility;
+
+import org.w3c.dom.Text;
+
+public class WeatherActivity extends Activity {
+    private LinearLayout weatherInfoLayout;
+    private TextView cityNameText;
+    private TextView publishText;
+    private TextView weatherDespText;
+    private TextView temp1Text;
+    private TextView temp2Text;
+    private TextView currentDateText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.weather_layout);
+        weatherInfoLayout = findViewById(R.id.weather_info_layout);
+        cityNameText = findViewById(R.id.city_name);
+        publishText = findViewById(R.id.publish_text);
+        weatherDespText = findViewById(R.id.weather_desp);
+        temp1Text = findViewById(R.id.temp1);
+        temp2Text = findViewById(R.id.temp2);
+        currentDateText = findViewById(R.id.current_date);
+        String countyCode = getIntent().getStringExtra("county_code");
+        if(!TextUtils.isEmpty(countyCode)){
+            // if have county level, then query weather
+            publishText.setText("synchronizing");
+            weatherInfoLayout.setVisibility(View.INVISIBLE);
+            cityNameText.setVisibility(View.INVISIBLE);
+            queryWeatherCode(countyCode);
+        }else{
+            //no countycode
+            showWeather();
+        }
+    }
+    /**
+     *query weather info of countyCode
+     */
+    private void queryWeatherCode(String countyCode){
+        String address = "http://www.weather.com.cn/data/list3/city"+countyCode+".xml";
+        queryFromServer(address, "countyCode");
+    }
+
+    /**
+     * query weather of weatherCode
+     */
+    private void queryWeatherInfo(String weatherCode){
+        String address = "http://www.weather.com.cn/data/cityinfo"+weatherCode+".html";
+        queryFromServer(address, "weatherCode");
+    }
+
+    /**
+     * query weaherCode or weatherInfo from server, according to address and type
+     */
+    private void queryFromServer(final String address, final String type){
+        HttpUtil.sendHttpRequest(address, new HttpCallbackListener() {
+            @Override
+            public void onFinish(String response) {
+                if("countyCode".equals(type)){
+                    if(!TextUtils.isEmpty(response)){
+                        //parse weatherCode from response
+                        String[] array = response.split("\\|");
+                        if(array != null && array.length == 2){
+                            String weatherCode = array[1];
+                            queryWeatherInfo(weatherCode);
+                        }
+
+                    }
+                }else if("weatherCode".equals(type)){
+                    //handle weatherInfo from server
+                    Utility.handleWeatherResponse(WeatherActivity.this,response);
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            showWeather();
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onError(Exception e) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        publishText.setText("synchronize fail");
+                    }
+                });
+            }
+        });
+    }
+
+    /**
+     * acquire weatherInfo from SharedPreference ,then show
+     */
+    private void showWeather(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        cityNameText.setText( prefs.getString("city_name", ""));
+        temp1Text.setText( prefs.getString("temp1", ""));
+        temp2Text.setText( prefs.getString("temp2", ""));
+        weatherDespText.setText(prefs.getString("weather_desp", ""));
+        publishText.setText("Today " + prefs.getString("publish_time","")+" publish");
+        currentDateText.setText( prefs.getString("current_date", ""));
+        weatherInfoLayout.setVisibility(View.VISIBLE);
+        cityNameText.setVisibility(View.VISIBLE);
+    }
+
+}
